d4584a981a5a3da42cc76f640e1345cd038e0a30
DYLAN OTINA <dylan.otina@gmail.com>
2022-01-15 18:57:03 -0800

    set up router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+import {Route, Routes} from 'react-router'
+import Home from "./pages/Home";
+import Browse from './pages/Browse'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/browse' element={<Browse/>} /> 
+      <Route path='/signin'  element={<SignIn/>} /> 
+      <Route path='/signup' element={<SignUp/>}/>  
+      </Routes>
     </div>
   );
 }
