c05e6347361d8c969b4363c7fe293b5a8621ca95
DYLAN OTINA <dylan.otina@gmail.com>
2022-01-17 09:38:17 -0800

    rename folder + change imports

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,5 @@
 export {default as Jumbotron} from './jumbotron'
-export {default as Accordeon} from './Accordeon'
+export {default as Accordeon} from './accordeon'
 export {default as Footer} from './footer'
 export {default as OptForm } from './optIn'
 export {default as Feature } from './feature'
